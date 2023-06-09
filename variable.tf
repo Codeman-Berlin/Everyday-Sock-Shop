b897f19a73b60af534f9a240afb7a36ef30e23df
Codeman-Berlin <codeman_devops@yahoo.com>
2023-06-09 20:38:46 +0200

    created the main.tf, variable.tf, output.tf, provider.tf and gitignore files.Also populated the vpc

---
+++ variable.tf
@@ -0,0 +1,33 @@
+variable "vpc_name" {
+  default = "AWS"
+}
+variable "name" {
+  default = "Sock-Shop"
+}
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+variable "az1" {
+  default = "eu-west-2a"
+}
+variable "az2" {
+  default = "eu-west-2b"
+}
+variable "prv-sn1" {
+  default = "10.0.1.0/24"
+}
+variable "prv-sn2" {
+  default = "10.0.2.0/24"
+}
+variable "prv-sn3" {
+  default = "10.0.3.0/24"
+}
+variable "prv-sn4" {
+  default = "10.0.4.0/24"
+}
+variable "pub-sn1" {
+  default = "10.0.5.0/24"
+}
+variable "pub-sn2" {
+  default = "10.0.6.0/24"
+}
